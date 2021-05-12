77b53285eea57995cd20f4e4b11e77424752d461
Wesley Gibbs <wcgibbsey@gmail.com>
2021-05-12 18:13:01 -0400

    Post updates

--- src/componenets/HeaderSearch.js
+++ src/componenets/HeaderSearch.js
@@ -3,14 +3,13 @@ import './Header.scss';
 function HeaderSearch() {
   return (
     <div className="header__search">
-    	<label className="visually-hidden" for="job-category-select">Job Categories</label>
-    	<select id="job-category-select" className="header__search-select">
-    		<option value="1">Select a Job category</option>
-    		<option value="1">1</option>
-    		<option value="2">2</option>
-    		<option value="3">3</option>
-    		<option value="4">4</option>
-    	</select>
+      <label className="visually-hidden" for="job-category-select">Job Categories</label>
+      <select id="job-category-select" className="header__search-select">
+        <option value="0">Select a Job category</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
     </div>
   );
 }
--- src/componenets/JobPost.js
+++ src/componenets/JobPost.js
@@ -4,60 +4,70 @@ function JobPost() {
   
   const posts = [
     {
+      category: '1',
       title: 'Job Post 1',
-      company: 'Company Name 1',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
-    {
+    { 
+      category: '1',
       title: 'Job Post 2',
-      company: 'Company Name 2',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
     {
+      category: '1',
       title: 'Job Post 3',
-      company: 'Company Name 3',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
     {
+      category: '2',
       title: 'Job Post 4',
-      company: 'Company Name 4',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
     {
+      category: '2',
       title: 'Job Post 5',
-      company: 'Company Name 5',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
     {
+      category: '2',
       title: 'Job Post 6',
-      company: 'Company Name 6',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
     {
+      category: '3',
       title: 'Job Post 7',
-      company: 'Company Name 7',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
     {
+      category: '3',
       title: 'Job Post 8',
-      company: 'Company Name 8',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
     {
+      category: '3',
       title: 'Job Post 9',
-      company: 'Company Name 9',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
     {
+      category: '3',
       title: 'Job Post 10',
-      company: 'Company Name 10',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      company: 'Company Name',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
     },
   ];
   
   return (
     posts.map((post) =>
-    <div className="job-post" key={post.title}>
+    <div className="job-post" key={post.title} data-category={post.category}>
       <h2 className="job-post__title">{post.title}</h2>
       <h3 className="job-post__company">{post.company}</h3>
       <hr className="job-post__title-divider" />
