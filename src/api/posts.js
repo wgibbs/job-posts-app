6edeeb51d6eeca4ebeb36a53f6d48712cb2e678e
Wesley Gibbs <wcgibbsey@gmail.com>
2021-05-12 20:41:42 -0400

    Separated posts data

---
+++ src/api/posts.js
@@ -0,0 +1,64 @@
+export const posts = [
+  {
+    category: '1',
+    title: 'Job Post 1',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+  { 
+    category: '1',
+    title: 'Job Post 2',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+  {
+    category: '1',
+    title: 'Job Post 3',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+  {
+    category: '2',
+    title: 'Job Post 4',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+  {
+    category: '2',
+    title: 'Job Post 5',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+  {
+    category: '2',
+    title: 'Job Post 6',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+  {
+    category: '3',
+    title: 'Job Post 7',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+  {
+    category: '3',
+    title: 'Job Post 8',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+  {
+    category: '3',
+    title: 'Job Post 9',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+  {
+    category: '3',
+    title: 'Job Post 10',
+    company: 'Company Name',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
+  },
+];
+
+export default posts;
--- src/componenets/JobPost.js
+++ src/componenets/JobPost.js
@@ -1,68 +1,6 @@
 import React from 'react';
+import {posts} from '../api/posts';
 import './JobPost.scss';
-
-const posts = [
-  {
-    category: '1',
-    title: 'Job Post 1',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-  { 
-    category: '1',
-    title: 'Job Post 2',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-  {
-    category: '1',
-    title: 'Job Post 3',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-  {
-    category: '2',
-    title: 'Job Post 4',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-  {
-    category: '2',
-    title: 'Job Post 5',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-  {
-    category: '2',
-    title: 'Job Post 6',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-  {
-    category: '3',
-    title: 'Job Post 7',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-  {
-    category: '3',
-    title: 'Job Post 8',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-  {
-    category: '3',
-    title: 'Job Post 9',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-  {
-    category: '3',
-    title: 'Job Post 10',
-    company: 'Company Name',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit et dolore magna aliqua.',
-  },
-];
 
 class JobPost extends React.Component {
   
