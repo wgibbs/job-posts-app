0e541d42a2d959a05e6ad19766112da133ff5cfb
Wesley Gibbs <wcgibbsey@gmail.com>
2021-05-19 18:31:08 -0400

    Added job highlights

--- src/componenets/JobPost.js
+++ src/componenets/JobPost.js
@@ -18,6 +18,12 @@ class JobPost extends React.Component {
             <h3 className="job-post__company">{post.company}</h3>
             <hr className="job-post__title-divider" />
             <p className="job-post__description">{post.desc}</p>
+            <ul className="job-post__highlights">
+              <li>{post.highlight1}</li> 
+              <li>{post.highlight2}</li> 
+              <li>{post.highlight3}</li>
+              <li>{post.highlight4}</li>
+            </ul>
             <a className="job-post__link" href="/">Learn More</a>
           </div>
           
