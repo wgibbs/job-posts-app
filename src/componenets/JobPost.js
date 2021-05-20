547d0c26bce1d31662296dd85453476b9863eb06
Wesley Gibbs <wcgibbsey@gmail.com>
2021-05-20 10:22:29 -0400

    Highlights updates

--- src/componenets/JobPost.js
+++ src/componenets/JobPost.js
@@ -6,27 +6,29 @@ class JobPost extends React.Component {
   render() {
     const posts = this.props.posts;
     const activeCategory = this.props.activeCategory;
-    
+
     return (
       <section className="job-post-container">
-        {posts.map((post) =>
+        {(posts).map((post, i) =>
           <div 
-            key={post.id}
+            key={i}
             className={'job-post' + (activeCategory === 'all' || activeCategory === post.category ? '' : ' hidden')}
           >
             <h2 className="job-post__title">{post.title}</h2>
             <h3 className="job-post__company">{post.company}</h3>
             <hr className="job-post__title-divider" />
             <p className="job-post__description">{post.desc}</p>
-            <ul className="job-post__highlights">
-              <li>{post.highlight1}</li> 
-              <li>{post.highlight2}</li> 
-              <li>{post.highlight3}</li>
-              <li>{post.highlight4}</li>
-            </ul>
+            <div className="job-post__highlights">
+              <h4>Highlights:</h4>
+              <ul>
+                <li>{post.highlight1}</li>
+                <li>{post.highlight2}</li>
+                <li>{post.highlight3}</li>
+                <li>{post.highlight4}</li>
+              </ul>
+            </div>
             <a className="job-post__link" href="/">Learn More</a>
           </div>
-          
         )}
       </section>
     );
